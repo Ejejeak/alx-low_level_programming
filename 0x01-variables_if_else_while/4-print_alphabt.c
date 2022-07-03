c3521786e1ea54933fb3cc31ae3272d882659361
Ejejeak <Ejejeak>
2022-07-03 08:36:58 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,15 +3,16 @@
  * main - Starting point
  * Return: always return 0
  */
+int main(void)
 {
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
-	{
+{
 	if (a == 'q' || a == 'e')
-	}
+{
 	continue;
-	}
+}
         putchar(a);
 	return (0);
 }
