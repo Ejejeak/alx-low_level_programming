66f3b2aa49c899f3e7d0dc50f032c1d71b3da848
Ejejeak <Ejejeak>
2022-07-03 13:23:38 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+int 1, 2;
+
+for (1 = 0; 1 < 9; 1++)
+{
+for (2 = 1 + 1; 2 < 10; 2++)
+{
+putchar((1 % 10) + '0');
+putchar((2 % 10) + '0');
+
+if (1 == 8 && 2 == 9
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
