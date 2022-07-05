5e45e63f28c3335f79e23147a097bc298b6df36e
Ejejeak <Ejejeak>
2022-07-05 13:52:21 -0700

    program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * main - Starting point
+ * main - print Holberton, followed by
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char *sh = "Main";
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
-while (*sh)
-{
-_putchar(*sh);
-sh++;
-}
-_putchar('\n');
-
-return (0);
+	return (0);
 }
